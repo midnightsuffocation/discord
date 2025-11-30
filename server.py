3de820951135283569922a8950272ccaadafe25a
midnightsuffocation <155326505+midnightsuffocation@users.noreply.github.com>
2025-11-30 11:37:56 +0100

    Updat

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from email.utils import formatdate
 
 PORT = 8000
 LOG_FILE = "access.log"
-DISCORD_WEBHOOK = ("https://discord.com/api/webhooks/1398083193265062020/OL-nBYHkOjSi8Nf50TJ9vA4UTyT6dhNcSTlF6vc4B-UEgmf8nHtnvMzj7DGd8iLiZDjt")  # Add your Discord webhook URL here
+DISCORD_WEBHOOK = ("https://canary.discord.com/api/webhooks/1444638456587358209/Uahc18vz6UeVyrI9V_qJ8rgymjFZOmUHGdNOA6_4op1CXC9TyYm86ITcTDvpNLJml5If")  # Add your Discord webhook URL here
 PIXEL_PATH = "/tracking_pixel.gif"
 PIXEL_DATA = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
 
